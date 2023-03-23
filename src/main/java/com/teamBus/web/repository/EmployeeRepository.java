65e4f0a0c9c41b868b726b3a588c29b55312b1aa
MoJaeYoung <jym2013@gmail.com>
2023-03-23 14:21:42 +0900

    Employee 완료

--- src/main/java/com/teamBus/web/repository/EmployeeRepository.java
+++ src/main/java/com/teamBus/web/repository/EmployeeRepository.java
@@ -10,8 +10,24 @@ import com.teamBus.web.entity.Employee;
 @Mapper
 public interface EmployeeRepository {
 	
+//기본형
+	// create
+	int insert(Employee emp);
+	
+	// read
 	List<Employee> findAll();
 	Employee findById(Integer id);
 	
+	// update
+	int update(Employee emp);
+	
+	// delete
+	int delete(Integer id);
+	
+
+//view
+	//HR관리자 계정에서내역조회 
     List<AdminListDayView> findViewByCompanyId(Integer companyId);
+    
+    
 }
---
+++ src/test/java/com/teamBus/web/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.teamBus.web.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
+import com.teamBus.web.entity.AdminListDayView;
+import com.teamBus.web.entity.Employee;
+
+
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@MybatisTest
+class EmployeeRepositoryTest {
+	
+	@Autowired
+	private EmployeeRepository repository;
+	
+
+//	@Test
+	void findTest() {
+		List <Employee> list = repository.findAll();
+		System.out.println(list);
+	}
+	
+//	@Test
+	void findByIdTest() {
+		
+		Employee em = repository.findById(10);
+		
+		System.out.println(em);
+	}
+	
+//	@Test
+	void findidViewTest() {
+		List <AdminListDayView> list = repository.findViewByCompanyId(1);
+		System.out.println(list);
+	}
+	
+//	@Test
+	void insertTest() {
+
+	
+		Employee em = new Employee
+			(null,1, //구분1  
+			"0000","박동조","01012323522","q@a.com", //구분2
+			null,null,null,null,null,null); //구분3
+		
+		int t = repository.insert(em);
+		System.out.println(t);
+		
+//		System.out.println(list);
+		
+	}
+	
+//	@Test
+	void updateTest() {
+
+		Employee em = new Employee
+			(10,1, //구분1  
+			"0000","박동조","01012323522","q@a.com", //구분2
+			null,null,null,null,null,null); //구분3
+		
+		int t = repository.insert(em);
+		System.out.println("update "+t);
+		
+//		System.out.println(list);
+		
+	}
+	
+	@Test
+	void deleteTest() {
+
+		
+		
+		int t = repository.delete(10);
+		System.out.println("delete "+t);
+		
+//		System.out.println(list);
+		
+	}
+	
+	
+	
+
+}
