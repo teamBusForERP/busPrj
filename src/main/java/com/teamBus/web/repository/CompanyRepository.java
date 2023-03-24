e031ca69da6b720e228c9aac1be711fcd3fdc06c
MoJaeYoung <jym2013@gmail.com>
2023-03-24 09:41:21 +0900

    company 중

--- src/main/java/com/teamBus/web/repository/CompanyRepository.java
+++ src/main/java/com/teamBus/web/repository/CompanyRepository.java
@@ -1,15 +1,25 @@
 package com.teamBus.web.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.teamBus.web.entity.Company;
+import com.teamBus.web.entity.Employee;
 
 @Mapper
 public interface CompanyRepository {
 	
-//	List<Company> findAll();
-	Company findById(int companyId);
-//	int insert(Company rcmdMenu);
-//	int update(Company rcmdMenu);
-//	int delete(int companyId);
+//기본형
+		// create
+		int insert(Company emp);
+		// read
+//		List<Company> findAll();
+		Company findById(int companyId);
+		// update
+		int update(Company emp);
+		// delete
+		int delete(Company id);
+		
+//view
 }
